5e610e6376722bc38f7fe04d301ee3e505118d22
Martarelli <renanmartarelli@gmail.com>
2025-07-01 16:19:25 -0300

    initial commit working host

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => loadRemoteModule('mfe1', './HomeComponent')
+      .then((m) => m.HomeComponent),
+  }
+];
